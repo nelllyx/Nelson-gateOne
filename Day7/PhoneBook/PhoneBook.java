4d7913eb0a856f9dc1039e240946e376ee79ffb0
nelllyx <nelsonakewe0@gmail.com>
2024-05-14 03:21:44 +0100

    python update

--- Day7/PhoneBook/PhoneBook.java
+++ Day7/PhoneBook/PhoneBook.java
@@ -16,9 +16,31 @@ private static ArrayList<Integer> phoneNumbers = new ArrayList<Integer>();
 
 Scanner input = new Scanner(System.in);
 
+String prompt = """
 
+Welcome to your phone book page
 
-mainMenu();
+Please select your preferred option
+
+--------------------------------------
+
+1 -> Add contact
+
+2 -> Remove contact
+
+3 -> Find contact by phone number
+
+4 -> Find contact by First name 
+
+5 -> Find contact by Last name
+
+6 -> Edit contact
+
+""";
+
+System.out.println(prompt);
+
+int userInput = input.nextInt();
 
 switch (userInput){
 
@@ -126,89 +148,13 @@ System.out.println("Saved Successfully!!");
 
 public static void removeContact(){
 
- 
+
 
 
 }
 
 public static void mainMenu(){
 
-Scanner input = new Scanner(System.in);
-
-String prompt = """
-
-Welcome to your phone book page
-
-Please select your preferred option
-
---------------------------------------
-
-1 -> Add contact
-
-2 -> Remove contact
-
-3 -> Find contact by phone number
-
-4 -> Find contact by First name 
-
-5 -> Find contact by Last name
-
-6 -> Edit contact
-
-""";
-
-System.out.println(prompt);
-
-int userInput = input.nextInt();
-
-
-switch (userInput){
-
-case 1:
-
-System.out.println("Create a new contact");
-
-boolean option = true;
-
-do{
-
-addContact();
-
-System.out.print("Do you want to save another contact? ");
-
-
-String userResponse = input.next();
-
-if (userResponse.equals("no")){
-
-option = false;
-}
-
-}
-
-while(option);
-
-break;
-
-case 2:
-
-System.out.print("Enter the first name of contact to delete: ");
-
-String name = input.next();
-
-System.out.print("Enter the phone number of contact to delete: ");
-
-int number = input.nextInt();
-
-System.out.println("Deleting >>>>>>>>>>>>>>>");
-
-System.out.println("Deleted Successfully!!");
-
-
-
-
-
-}
 
 
 
--- Day7/PhoneBook/phone_book.py
+++ Day7/PhoneBook/phone_book.py
@@ -1,4 +1,12 @@
-prompt = """
+first_name_list = []
+
+last_name_list = []
+
+phone_number_list = []
+
+def main_menu():
+
+	prompt = """
 
 Welcome to your phone book page
 
@@ -18,17 +26,121 @@ Please select your preferred option
 
 6 -> Edit contact
 
-
 """
 
-user_response = int(input(prompt))
+	user_response = int(input(prompt))
 
-match user_response:
+	display_options(user_response)
 
-	case first:
+def add_contact():
 
-		print("Create a new contact")
+	print("Create a new contact")
 
-		option = True
+	first_name = input("Enter first name: ")
+
+	first_name_list.append(first_name)
+
+	last_name = input("Enter last name: ")
+
+	last_name_list.append(last_name)
+
+	phone_number = int(input("Enter user phone number: "))
+
+	phone_number_list.append(phone_number)
+
+	print("Saving >>>>>>>>>>>>>>>>>")
+
+	print("Saved successfully")
+
+	user_answer = input("Do you want to save another contact: ")
+
+	if user_answer == "yes":
+
+		add_contact()
+		
+	elif user_answer == "no":
+	
+		response = input("Press 0 to go back to main menu or 1 to exit: ")
+
+		if response == "0":
+
+			main_menu()
+
+		elif response == "1":
+				
+			print("Thank you")
+	
+def remove_contact():
+
+	delete_first_name = input("Enter contact name you wish to delete: ")
+
+	for i in range(len(first_name_list)-1):
+
+			if delete_first_name == first_name_list[i]:
+
+				first_name_list.pop(i)
+
+				last_name_list.pop(i)
+
+				phone_number_list.pop(i)
+
+				print("Deleting >>>>>>>>>>>>>>>>>")
+
+				print("Deleted successfully")
+
+				print(first_name_list)
+
+			
+def find_by_number():
+
+	user_number = int(input("Enter the number you wish to find: "))
+
+	for index in range(len(phone_number_list)-1):
+
+		if user_number == phone_number_list[index]:
+
+			first = first_name_list[index]
+
+			second = last_name_list[index]
+
+			print (user_number, first,second, end = "")
+
+			
+
+
+
+def display_options(user_response):
+
+	match user_response:
+
+		case 1: add_contact()
+
+		case 2: remove_contact()
+
+		#case 3: find_by_number()
+
+	
+
+
+
+main_menu()
+
+		
+
+#print(phone_number_list)
+
+#print(last_name_list)
+
+#print(first_name_list)
+
+
+
+
+
+
+
+
+
+
 
 		
